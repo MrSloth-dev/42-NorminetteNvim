ba6a8eda3584bc8be836f83369839dda66f16c4e
MrSloth <joao8barbosa@gmail.com>
2024-09-07 15:25:28 +0100

    Bugfix when auto_run was enabled

--- lua/norminette/init.lua
+++ lua/norminette/init.lua
@@ -4,6 +4,8 @@ M.version = "0.3"
 
 M.dependencies = { "nvim-lua/plenary.nvim" }
 
+M.auto_run = false
+
 local has_plenary, async = pcall(require, "plenary.async")
 if not has_plenary then
 	error("This plugin requires plenary.nvim. Please install it to use this plugin.")
@@ -45,25 +47,33 @@ local function diagnostics_exist(namespace, bufnr)
 	return #diagnostics > 0
 end
 
+local function run_norminette_check(bufnr, namespace)
+	vim.cmd("write")
+	local filename = vim.api.nvim_buf_get_name(bufnr)
+	async.run(function()
+		local output = vim.fn.system("norminette " .. vim.fn.shellescape(filename))
+		return output
+	end, function(output)
+		local diagnostics = parse_norminette_output(output)
+		vim.schedule(function()
+			vim.diagnostic.reset(namespace, bufnr)
+			vim.diagnostic.set(namespace, bufnr, diagnostics)
+		end)
+	end)
+end
+
 M.run_norminette = async.void(function()
 	local bufnr = vim.api.nvim_get_current_buf()
-	local filename = vim.api.nvim_buf_get_name(bufnr)
 	local namespace = vim.api.nvim_create_namespace("norminette")
-	if diagnostics_exist(namespace, bufnr) then
-		clear_diagnostics(namespace, bufnr)
+	if M.auto_run then
+		run_norminette_check(bufnr, namespace)
 	else
-		vim.cmd("write")
-
-		async.run(function()
-			local output = vim.fn.system("norminette " .. vim.fn.shellescape(filename))
-			return output
-		end, function(output)
-			local diagnostics = parse_norminette_output(output)
-			vim.schedule(function()
-				vim.diagnostic.reset(namespace, bufnr)
-				vim.diagnostic.set(namespace, bufnr, diagnostics)
-			end)
-		end)
+		if diagnostics_exist(namespace, bufnr) then
+			clear_diagnostics(namespace, bufnr)
+			run_norminette_check(bufnr, namespace)
+		else
+			run_norminette_check(bufnr, namespace)
+		end
 	end
 end)
 
